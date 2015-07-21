c6d6d29634ba97a83d51afd8cc257446d03c5192
sudosurootdev <sudosurootdev@gmail.com>
2015-07-21 01:05:29 -0400

    Fix f400 build by moving to f400k since CM's device repo calls f400k
    Change-Id: I28e79ceb0c052b6be27e8840f45711130aca74ac
    Signed-off-by: sudosurootdev <sudosurootdev@gmail.com>

--- f400k/f400-vendor.mk
+++ f400k/f400-vendor.mk
@@ -16,4 +16,4 @@
 
 # Pick up overlay for features that depend on non-open-source files
 
-$(call inherit-product, vendor/lge/f400/f400-vendor-blobs.mk)
+$(call inherit-product, vendor/lge/f400k/f400k-vendor-blobs.mk)
